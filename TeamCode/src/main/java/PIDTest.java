7848589c0af2e8cccf80bd4206417508f2549c66
HSLegacy <henryseefeldt@proton.me>
2025-08-21 13:55:57 -0500

    changed everything

--- TeamCode/src/main/java/PIDTest.java
+++ TeamCode/src/main/java/PIDTest.java
@@ -1,30 +1,68 @@
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.rowanmcalpin.nextftc.core.command.groups.ParallelGroup;
 import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
+import com.rowanmcalpin.nextftc.core.command.utility.LambdaCommand;
 import com.rowanmcalpin.nextftc.core.command.utility.delays.Delay;
 import com.rowanmcalpin.nextftc.ftc.NextFTCOpMode;
 import com.rowanmcalpin.nextftc.core.command.Command;
+import com.rowanmcalpin.nextftc.core.units.Distance;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+
 
 @Autonomous(name = "PIDTest")
 public class PIDTest extends NextFTCOpMode {
+    Limelight3A limelight;
+
+    @Override
+    public void onInit() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number 0
+    }
+
     public PIDTest() {
         super(Turret.INSTANCE);
     }
 
-    public Command firstRoutine() {
+
+
+    public Command firstRoutine(double clicks) {
         return new SequentialGroup(
-                Turret.INSTANCE.toHigh(),
-                new ParallelGroup(
-                        Turret.INSTANCE.toMiddle()
-                ),
-                new Delay(0.5),
-                new ParallelGroup(
-                )
+                Turret.INSTANCE.run(clicks)
         );
     }
 
+
+
     @Override
     public void onStartButtonPressed() {
-        firstRoutine().invoke();
+
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            firstRoutine(tx).invoke();
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+
+        telemetry.addData("motor Position:", Turret.INSTANCE.motor.getCurrentPosition());
+        telemetry.update();
     }
 }
+
--- TeamCode/src/main/java/Turret.java
+++ TeamCode/src/main/java/Turret.java
@@ -1,45 +1,47 @@
+import static com.rowanmcalpin.nextftc.ftc.OpModeData.telemetry;
+
+import com.acmerobotics.dashboard.config.Config;
 import com.rowanmcalpin.nextftc.core.Subsystem;
 import com.rowanmcalpin.nextftc.core.command.Command;
+import com.rowanmcalpin.nextftc.core.control.controllers.Controller;
 import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
 import com.rowanmcalpin.nextftc.core.control.controllers.feedforward.StaticFeedforward;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.HoldPosition;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition;
 
+@Config
 public class Turret extends Subsystem {
     // BOILERPLATE
     public static final Turret INSTANCE = new Turret();
     private Turret() { }
 
-    // USER CODE
-    public MotorEx motor;
+        // USER CODE
+        public MotorEx motor;
 
-    public PIDFController controller = new PIDFController(0.005, 0.0, 0.0, new StaticFeedforward(0.0));
+        public static double kP = 0.05;
+        public static double kI = 0;
+        public static double kD = 0;
 
-    public String name = "lift_motor";
+        public PIDFController controller = new PIDFController(kP, kI, kD, new StaticFeedforward(0.0));
 
-    public Command toLow() {
-        return new RunToPosition(motor, // MOTOR TO MOVE
-                0.0, // TARGET POSITION, IN TICKS
-                controller, // CONTROLLER TO IMPLEMENT
-                this); // IMPLEMENTED SUBSYSTEM
-    }
+        public String name = "motor";
 
-    public Command toMiddle() {
-        return new RunToPosition(motor, // MOTOR TO MOVE
-                500.0, // TARGET POSITION, IN TICKS
-                controller, // CONTROLLER TO IMPLEMENT
-                this); // IMPLEMENTED SUBSYSTEM
-    }
+        public Command run ( double clicks){
+            telemetry.addData("clicks: ", clicks);
+            return new RunToPosition(motor, // MOTOR TO MOVE
+                    motor.getCurrentPosition() + clicks, // TARGET POSITION, IN TICKS
+                    controller, // CONTROLLER TO IMPLEMENT
+                    this); // IMPLEMENTED SUBSYSTEM
+        }
 
-    public Command toHigh() {
-        return new RunToPosition(motor, // MOTOR TO MOVE
-                1200.0, // TARGET POSITION, IN TICKS
-                controller, // CONTROLLER TO IMPLEMENT
-                this); // IMPLEMENTED SUBSYSTEM
-    }
+        @Override
+        public void initialize () {
+            motor = new MotorEx(name);
+        }
 
-    @Override
-    public void initialize() {
-        motor = new MotorEx(name);
-    }
+        @Override
+        public Command getDefaultCommand () {
+            return new HoldPosition(motor, controller, this);
+        }
 }
