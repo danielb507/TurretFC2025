adf689fe36cfa89d11a705a123ee2cc59a7a7bd2
HSLegacy <henryseefeldt@proton.me>
2025-09-15 10:06:37 -0500

    initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTuner.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.graph.GraphManager;
+import com.bylazar.graph.PanelsGraph;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import dev.nextftc.core.components.*;
+
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+
+@Configurable
+@Autonomous(name = "PIDTuner")
+public class PIDTuner extends NextFTCOpMode {
+    Limelight3A limelight;
+
+    @Override
+    public void onInit() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number 0
+    }
+
+
+    @Override
+    public void onUpdate() {
+        LLResult result = limelight.getLatestResult();
+        if (result != null) {
+            if (result.isValid()) {
+                telemetry.addData("ty", result.getTy());
+            }
+        }
+    }
+}
+
