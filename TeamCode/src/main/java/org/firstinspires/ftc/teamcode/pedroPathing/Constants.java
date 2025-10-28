6d624d0d33d704c17613abd13c22808d7bf726c0
levir <levi.ryza@ums.legacypca.org>
2025-10-28 14:53:09 -0500

    Tuning robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -17,14 +17,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-        .mass(5.98)
-        .forwardZeroPowerAcceleration(-35.5019)
-        .lateralZeroPowerAcceleration(-48.3435)
-        .translationalPIDFCoefficients(new PIDFCoefficients(0.06, 0, 0.005, .025))
-        .headingPIDFCoefficients(new PIDFCoefficients(.8, 0, 0.04, 0.025 ))
-        .centripetalScaling(0.001)
-        .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.01,0.0,0.0,1,0.045));
-    //centripical here
+        .mass(8.799692)
+        .forwardZeroPowerAcceleration(-44.584)
+        .lateralZeroPowerAcceleration(-66.0535)
+        .translationalPIDFCoefficients(new PIDFCoefficients(0.07, 0, 0.008, 0.025));
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -32,20 +28,20 @@ public class Constants {
             .rightRearMotorName("rr")
             .leftRearMotorName("lr")
             .leftFrontMotorName("lf")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(66.8102)
-            .yVelocity(53.3095);
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(56.7638)
+            .yVelocity(46.2868);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(3.5)
-            .strafePodX(-8)
+            .forwardPodY(-5.5)
+            .strafePodX(-7)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
